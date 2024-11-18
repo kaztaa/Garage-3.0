4dbf21a8186e03ead47765fe1573604efc2d35e8
dzta <dzta@protonmail.com>
2024-11-18 15:03:42 +0100

    Vehicle Class

---
+++ Garage 3.0/Models/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿namespace Garage_3._0.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string RegistrationNumber { get; set; } = string.Empty;
+        public string Color { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public int ParkingSpotSize { get; set; }
+        public int NumberOfWheels { get; set; }
+        public int VehicleTypeId { get; set; }
+        // public virtual VehicleType? VehicleType { get; set; }
+
+
+    }
+}
