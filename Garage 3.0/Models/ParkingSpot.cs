f95b47025a13adead3d5736e7e3b67cf5cdd9077
bldzz <boldzz@hotmail.com>
2024-11-18 15:18:09 +0100

    Added ParkingSpot model

---
+++ Garage 3.0/Models/ParkingSpot.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class ParkingSpot
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string SpotNumber { get; set; } = string.Empty;
+
+    [Required]
+    public bool IsOccupied { get; set; }
+
+    public int ParkingSpotSize {  get; set; }
+    public int? VehicleId { get; set; } // Nullable for empty spots
+    
+
+}
